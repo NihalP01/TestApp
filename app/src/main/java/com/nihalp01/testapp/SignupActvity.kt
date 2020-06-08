cac64aad6882b4a4e6e24be49f66ff752321eb8b
NihalP01 <nihalburagohain02@gmail.com>
2020-06-08 15:32:38 +0530

    Initial commit

---
+++ app/src/main/java/com/nihalp01/testapp/SignupActvity.kt
@@ -0,0 +1,12 @@
+package com.nihalp01.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
