82d3094665c81bd8ff0e49647fb41eeebfc3382f
NihalP01 <nihalburagohain02@gmail.com>
2020-06-08 15:52:17 +0530

    updating

--- app/src/main/java/com/nihalp01/testapp/LoginActivity.kt
+++ app/src/main/java/com/nihalp01/testapp/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.nihalp01.testapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -9,5 +11,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+        create_account.setOnClickListener {
+            startActivity(Intent(this, SignupActvity::class.java))
+        }
     }
 }
